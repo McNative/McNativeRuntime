5df7167d87e1f40f019d84db8458615326a8d0ec
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-12-30 20:51:54 +0100

    Updated default event implementation

--- mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/event/service/local/DefaultLocalServiceStartupEvent.java
+++ mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/event/service/local/DefaultLocalServiceStartupEvent.java
@@ -1,5 +1,6 @@
 package org.mcnative.runtime.common.event.service.local;
 
 import org.mcnative.runtime.api.event.service.local.LocalServiceEvent;
+import org.mcnative.runtime.api.event.service.local.LocalServiceStartupEvent;
 
-public class DefaultLocalServiceStartupEvent implements LocalServiceEvent {}
+public class DefaultLocalServiceStartupEvent implements LocalServiceStartupEvent {}
