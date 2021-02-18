1f70d7d443631f17d031e46780f9294be9d85d88
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-02-18 15:57:02 +0100

    Optimized McNative action framework connection

--- mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/maf/MAFService.java
+++ mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/maf/MAFService.java
@@ -1,5 +1,7 @@
 package org.mcnative.runtime.common.maf;
 
+import net.pretronic.libraries.utility.SystemUtil;
+import net.pretronic.libraries.utility.exception.OperationFailedException;
 import net.pretronic.libraries.utility.interfaces.ObjectOwner;
 import org.mcnative.actionframework.sdk.client.MAFClient;
 import org.mcnative.actionframework.sdk.client.discovery.DnsServiceDiscovery;
@@ -13,26 +15,32 @@ import java.util.UUID;
 public class MAFService {
 
     public static void start(){
-        McNativeConsoleCredentials credentials = McNative.getInstance().getConsoleCredentials();
-        MAFStatusListener statusListener = new MAFStatusListener();
-        MAFClient client = MAFClient.build()
-                .serviceDiscovery(new DnsServiceDiscovery("_maf._tcp.mcnative.org"))
-                .authentication(new KeyAuthentication(UUID.fromString(credentials.getNetworkId()),credentials.getSecret()))
-                .logger(McNative.getInstance().getLogger())
-                .autoReconnect(1000)
-                .statusListener(statusListener)
-                .uniqueId(McNative.getInstance().getLocal().getUniqueId())
-                .name(McNative.getInstance().getLocal().getName())
-                .type(ClientType.GENERIC)
-                .create();
-
-        try{
-            client.connect();
-            statusListener.setClient(client);
-            McNative.getInstance().getLocal().getEventBus().subscribe(ObjectOwner.SYSTEM,new MAFListener(client));
-        }catch (Exception exception){
-            McNative.getInstance().getLogger().info("[MAF] Could not connect to McNative action framework ("+exception.getMessage()+")");
-        }
+        McNative.getInstance().getScheduler().createTask(ObjectOwner.SYSTEM).name("MAF").async().execute(() -> {
+            McNativeConsoleCredentials credentials = McNative.getInstance().getConsoleCredentials();
+            MAFStatusListener statusListener = new MAFStatusListener();
+            MAFClient client = MAFClient.build()
+                    .serviceDiscovery(new DnsServiceDiscovery("_maf._tcp.mcnative.org"))
+                    .authentication(new KeyAuthentication(UUID.fromString(credentials.getNetworkId()),credentials.getSecret()))
+                    .logger(McNative.getInstance().getLogger())
+                    .autoReconnect(1000)
+                    .statusListener(statusListener)
+                    .uniqueId(McNative.getInstance().getLocal().getUniqueId())
+                    .name(McNative.getInstance().getLocal().getName())
+                    .type(ClientType.GENERIC)
+                    .create();
+            for (int i = 0; i < 3; i++) {
+                try{
+                    client.connect();
+                    statusListener.setClient(client);
+                    McNative.getInstance().getLocal().getEventBus().subscribe(ObjectOwner.SYSTEM,new MAFListener(client));
+                    return;
+                }catch (Exception exception){
+                    McNative.getInstance().getLogger().info("[MAF] Could not connect to McNative action framework ("+exception.getMessage()+")");
+                    if(exception instanceof OperationFailedException) return;
+                }
+                SystemUtil.sleepUninterruptible(1500);
+            }
+        });
     }
 
 }
