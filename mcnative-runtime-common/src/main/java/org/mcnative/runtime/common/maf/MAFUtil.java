7aeb38a79912f6a2511913ac0d88813a594ad941
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-03-05 22:31:45 +0100

    Optimized maf connection

--- mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/maf/MAFUtil.java
+++ mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/maf/MAFUtil.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 public class MAFUtil {
 
     protected static void sendStartupAction(MAFClient client){
+        System.out.println("preparing: "+client.getConnection().isConnected());
         if(!client.getConnection().isConnected()) return;
         McNative runtime = McNative.getInstance();
         MinecraftPlatform platform =  runtime.getPlatform();
