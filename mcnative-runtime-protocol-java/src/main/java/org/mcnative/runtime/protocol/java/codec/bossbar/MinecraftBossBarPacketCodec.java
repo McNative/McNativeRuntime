1dc657bd7741be0ca99ac02f4cbc436ad1950d18
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-03-14 15:42:59 +0100

    Fixed an issue in 1.9> versions

--- mcnative-runtime-protocol-java/src/main/java/org/mcnative/runtime/protocol/java/codec/bossbar/MinecraftBossBarPacketCodec.java
+++ mcnative-runtime-protocol-java/src/main/java/org/mcnative/runtime/protocol/java/codec/bossbar/MinecraftBossBarPacketCodec.java
@@ -12,7 +12,8 @@ public class MinecraftBossBarPacketCodec {
     public static void register(PacketManager manager){
         manager.registerPacket(create(MinecraftBossBarPacket.class
                 ,on(PacketDirection.OUTGOING
-                        ,map(MinecraftProtocolVersion.JE_1_7,0x0C,new MinecraftBossBarPacketCodecV1_7())
+                        ,map(MinecraftProtocolVersion.JE_1_7,0,new MinecraftBossBarPacketCodecV1_7())
+                        ,map(MinecraftProtocolVersion.JE_1_9,0x0C,new MinecraftBossBarPacketCodecV1_9())
                         ,map(MinecraftProtocolVersion.JE_1_15,0x0D)
                         ,map(MinecraftProtocolVersion.JE_1_16,0x0C))));
     }
