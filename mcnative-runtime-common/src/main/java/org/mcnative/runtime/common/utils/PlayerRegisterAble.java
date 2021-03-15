b18705d50b219a5edb20a49644f25d5e1a3c5d4f
Fridious <philippfriedhoff@outlook.de>
2021-03-15 18:00:49 +0100

    Added utility interface PlayerRegisterAble

---
+++ mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/utils/PlayerRegisterAble.java
@@ -0,0 +1,8 @@
+package org.mcnative.runtime.common.utils;
+
+import org.mcnative.runtime.api.player.ConnectedMinecraftPlayer;
+
+public interface PlayerRegisterAble {
+
+    void registerPlayer(ConnectedMinecraftPlayer player);
+}
