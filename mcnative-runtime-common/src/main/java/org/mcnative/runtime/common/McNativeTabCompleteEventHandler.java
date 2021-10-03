7c37e6a9926720d377dc8edcf048472b87f44bdd
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-10-03 12:06:25 +0200

    Removed debugs

--- mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/McNativeTabCompleteEventHandler.java
+++ mcnative-runtime-common/src/main/java/org/mcnative/runtime/common/McNativeTabCompleteEventHandler.java
@@ -22,6 +22,7 @@ package org.mcnative.runtime.common;
 
 import net.pretronic.libraries.event.EventBus;
 import net.pretronic.libraries.utility.Iterators;
+import org.mcnative.runtime.api.McNative;
 import org.mcnative.runtime.api.connection.PendingConnection;
 import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteEvent;
 import org.mcnative.runtime.api.event.player.MinecraftPlayerTabCompleteResponseEvent;
@@ -37,7 +38,7 @@ import org.mcnative.runtime.api.protocol.packet.type.player.complete.MinecraftPl
 import org.mcnative.runtime.common.event.player.complete.DefaultMinecraftPlayerTabCompleteEvent;
 import org.mcnative.runtime.common.event.player.complete.DefaultMinecraftPlayerTabCompleteResponseEvent;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -46,10 +47,12 @@ public class McNativeTabCompleteEventHandler implements MinecraftPacketListener 
 
     private final EventBus eventBus;
     private final Map<UUID,String> cursors;
+    private final boolean interceptBaseCompletion;
 
-    public McNativeTabCompleteEventHandler(EventBus eventBus, PacketManager packetManager) {
+    public McNativeTabCompleteEventHandler(EventBus eventBus, PacketManager packetManager, boolean interceptBaseCompletion) {
         this.eventBus = eventBus;
         this.cursors = new ConcurrentHashMap<>();
+        this.interceptBaseCompletion = interceptBaseCompletion;
 
         packetManager.registerPacketListener(Endpoint.UPSTREAM, PacketDirection.INCOMING, MinecraftPlayerTabCompletePacket.class,this);
         packetManager.registerPacketListener(Endpoint.UPSTREAM, PacketDirection.OUTGOING, MinecraftPlayerTabCompleteResponsePacket.class,this);
@@ -70,13 +73,22 @@ public class McNativeTabCompleteEventHandler implements MinecraftPacketListener 
             if(mcnativeEvent.isCancelled()) event.setCancelled(true);
             else event.setRewrite(true);
 
+            if(this.interceptBaseCompletion && !mcnativeEvent.isCancelled() && packet.getCursor() != null
+                    && packet.getCursor().charAt(0) == '/' && packet.getCursor().indexOf(' ') < 0){
+                event.setCancelled(true);
 
-            MinecraftPlayerTabCompleteResponsePacket packet1 = new MinecraftPlayerTabCompleteResponsePacket();
-            packet1.setSuggestions(Arrays.asList("test1","test2","test3","test4"));
-            packet1.setTransactionId(packet.getTransactionId());
-            packet1.setLength(1);
-            packet1.setStart(1);
-            event.getConnection().sendPacket(packet1);
+                String rawCursor = packet.getCursor().substring(1).trim().toLowerCase();
+                List<String> suggestions = Iterators.map(McNative.getInstance().getLocal().getCommandManager().getCommands()
+                        ,command -> command.getConfiguration().getName().toLowerCase()
+                        ,command -> command.getConfiguration().getName().toLowerCase().startsWith(rawCursor));
+
+                MinecraftPlayerTabCompleteResponsePacket responsePacket = new MinecraftPlayerTabCompleteResponsePacket();
+                responsePacket.setSuggestions(suggestions);
+                responsePacket.setTransactionId(packet.getTransactionId());
+                responsePacket.setLength(packet.getCursor().length());
+                responsePacket.setStart(0);
+                event.getConnection().sendPacket(responsePacket);
+            }
 
         }else if(event.getPacket().getClass().equals(MinecraftPlayerTabCompleteResponsePacket.class)){
             MinecraftPlayerTabCompleteResponsePacket packet = event.getPacket(MinecraftPlayerTabCompleteResponsePacket.class);
